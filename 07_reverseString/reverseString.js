00a74bd24977b69c9cc460b64f853522a97414e2
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-05-11 20:35:45 -0400

    Complete 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    const array = [];
+    for (let i = string.length; i >= 0; i--) {
+        array.push(string[i]);
+    }
 
+    return array.join("");
 };
+
+let string = "";
+
+reverseString(string);
 
 // Do not edit below this line
 module.exports = reverseString;
