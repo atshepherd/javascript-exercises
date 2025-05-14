3835d5906414a8878c5d978ee1c0d53c0ccb4df3
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-05-13 23:28:25 -0400

    Pass test 3

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -2,10 +2,10 @@ const removeFromArray = function(array = []) {
         
     for(j=1; j<arguments.length; j++) {
         for (let i=0; i<array.length; i++) {
-            if (array[i] == arguments[j]) {
+            while (array[i] == arguments[j]) {
                 array.splice(i,1);
             }
-            else if(array[i] != arguments[j]) {
+            if (array[i] != arguments[j]) {
                 continue;
             }
         }
