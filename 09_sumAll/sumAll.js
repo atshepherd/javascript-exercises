0a45882cfd9dab5d589b028ada0411473a0df3ee
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-05-15 13:50:40 -0400

    Pass test 1

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
-
+const sumAll = function(firstNumber, secondNumber) {
+    let sum = 0;
+    
+    for (i=firstNumber; i<=secondNumber; i++) {
+        let nextNumber = firstNumber++;
+        sum = firstNumber + nextNumber;
+    }
+        return sum;
 };
 
 // Do not edit below this line
