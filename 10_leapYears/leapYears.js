691a1fb4aa80d13d1997ac7f762c818ba42a9e0b
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-05-15 22:48:55 -0400

    Complete 10_leapYears

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4 == 0 && year % 100 !== 0) {
+        return true;
+    }
+    else if (year % 4 == 0 && year % 100 == 0) {
+        if (year % 400 == 0) {
+            return true;
+        }
+    }
+    return false;
 };
 
 // Do not edit below this line
 module.exports = leapYears;
+
+// has to be divisible by 4
+// and not divisible by 100
+// if divisible by 100 must also be divisible by 400
