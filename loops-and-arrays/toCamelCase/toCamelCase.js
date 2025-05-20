db147db406a47927979e0e11f45796becc22b1b4
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-05-20 14:26:58 -0400

    Complete array challenge 1

--- loops-and-arrays/toCamelCase/toCamelCase.js
+++ loops-and-arrays/toCamelCase/toCamelCase.js
@@ -1,5 +1,11 @@
 function camelize(str) {
+    // split the string into an array, separated by dashes
     let splitArr = str.split("-");
+    // loop through the array, starting at the second word onwards
+    for (let i = 1; i < splitArr.length; i++) {
+        // take the first letter of the second word, make it uppercase, then add the rest of the word
+        splitArr[i] = splitArr[i].charAt(0).toUpperCase() + splitArr[i].slice(1);
+    }
+    // join the array into a string and return it
+    return splitArr.join("");
 }
-
-camelize("background-color");
