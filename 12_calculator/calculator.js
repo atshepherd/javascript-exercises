75e37548fba9c194b95ecda1c730bcf646a58049
alex <23156287+atshepherd@users.noreply.github.com>
2025-06-30 16:10:51 -0400

    Complete factorial function

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -16,14 +16,26 @@ const multiply = function(arr) {
 
 const power = function(num1, num2) {
   let sum = num1;
-	for (i = 1; i < num2; i++) {
+	for (let i = 1; i < num2; i++) {
     sum = sum * num1;
   }
   return sum;
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  let sum = 0;
+	if (num1 == 0 || num1 == 1) {
+      sum = 1;
+  }
+
+  else if (num1 != 0) {
+      let nextNum = num1 - 1;
+      sum = num1 * nextNum;
+    for (let i = (nextNum - 1); i > 0; i--) {
+        sum = (sum * i);
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
