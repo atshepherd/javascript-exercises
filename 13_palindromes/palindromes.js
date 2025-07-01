b1ad70c7b4f2a36d4cbe9ee3b242ad225998fb71
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-07-01 12:32:44 -0400

    Pass first test case

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    let arr = [];
+    for (i=0; i<string.length; i++) {
+        arr[i] = string[i];
+    }
+    let reverseArr=[];
+    j = 0;
+    for (i=(arr.length - 1); i >= 0; i--) {
+        reverseArr[j] = arr[i];
+        j++;
+    }
+    
+    for(i=0; i<arr.length; i++) {
+        if(arr[i] != reverseArr[i]) {
+            return false;
+        }
+    }
+    return true;
 };
 
 // Do not edit below this line
