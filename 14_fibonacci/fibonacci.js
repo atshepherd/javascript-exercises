e70351769bfb49603698e085856ec8bcbe64f373
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-07-03 12:10:44 -0400

    Complete 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(input) {
+    let num = Number(input);
+    if (num < 0) {
+        return "OOPS";
+    }
+    else {
+        let arr = [0, 1];
+        let prevSum;
+        for(let i = 2; i <= num; i++) {
+            prevNum1 = arr[i - 1];
+            prevNum2 = arr[i - 2];            
+            prevSum = prevNum1 + prevNum2;
+            arr[i] = prevSum;
+        }
+        return arr[num];
+    }
 };
 
 // Do not edit below this line
