c47bc4aba21a49e96890c5b05e36d3a297d3f63b
atshepherd <23156287+atshepherd@users.noreply.github.com>
2025-07-07 10:08:54 -0400

    Complete 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.sort((a,b) => {
+        if (a.yearOfDeath === undefined) {
+            return (2025 - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+        }
 
+        else if (b.yearOfDeath === undefined) {
+            return (a.yearOfDeath - a.yearOfBirth) - (2025 - b.yearOfBirth);
+        }
+
+        else {
+            return (a.yearOfDeath - a.yearOfBirth) - (b.yearOfDeath - b.yearOfBirth);
+        }
+    });
+
+    return people[people.length - 1];
 };
 
 // Do not edit below this line
